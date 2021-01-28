1c3ae994ebbd6d1869678a8477ff3f81533ed708
angelicafreitas <angelicamfreitas09@gmail.com>
2021-01-28 20:09:56 +0000

    mongo query 7

--- mongo/queries.js
+++ mongo/queries.js
@@ -1,38 +1,37 @@
 /* 
 docker start b87
-docker exec -it b87 bash
 nano hr.json
 copiar conteúdo ficheiro
 docker cp hr.json b87fb41cd521:/hr.json
+docker exec -it b87 bash
 mongo
 use hr
 db.createCollection("hrs")
+exit
 mongoimport -d hr -c hrs hr.json --jsonArray
  */
 
-
 //1 insert employee
 var employee = {
-    "EMPLOYEE_ID": 1049,
-    "FIRST_NAME": "Alexander",
-    "LAST_NAME": "Hunold",
-    "EMAIL": "AHUNOLD", 
-    "PHONE_NUMBER": "590.423.4567", 
-    "HIRE_DATE": "2006-01-03T00:00:00", 
-    "SALARY": 9000, 
+    "EMPLOYEE_ID": 12345, 
+    "FIRST_NAME": "Steven", 
+    "LAST_NAME": "King", 
+    "EMAIL": "SKING", 
+    "PHONE_NUMBER": "515.123.4567", 
+    "HIRE_DATE": "2003-06-17T00:00:00", 
+    "SALARY": 24000, 
     "COMMISSION_PCT": "null", 
-    "MANAGER_ID": 102, 
     "DEPARTMENT": {
-        "DEPARTMENT_ID": 60, 
-        "DEPARTMENT_NAME": "IT", 
-        "MANAGER_ID": 103, 
-        "LOCATION_ID": 1400, 
+        "DEPARTMENT_ID": 90, 
+        "DEPARTMENT_NAME": "Executive", 
+        "MANAGER_ID": 100, 
+        "LOCATION_ID": 1700, 
         "LOCATION": {
-            "LOCATION_ID": 1400, 
-            "STREET_ADDRESS": "2014 Jabberwocky Rd", 
-            "POSTAL_CODE": "26192", 
-            "CITY": "Southlake", 
-            "STATE_PROVINCE": "Texas", 
+            "LOCATION_ID": 1700, 
+            "STREET_ADDRESS": "2004 Charade Rd", 
+            "POSTAL_CODE": "98199", 
+            "CITY": "Seattle", 
+            "STATE_PROVINCE": "Washington", 
             "COUNTRY_ID": "US", 
             "COUNTRY": {
                 "COUNTRY_ID": "US", 
@@ -46,12 +45,13 @@ var employee = {
         }
     }, 
     "JOB": {
-        "JOB_ID": "IT_PROG", 
-        "JOB_TITLE": "Programmer", 
-        "MIN_SALARY": 4000, 
-        "MAX_SALARY": 10000
+        "JOB_ID": "AD_PRES", 
+        "JOB_TITLE": "President", 
+        "MIN_SALARY": 20080, 
+        "MAX_SALARY": 40000
     }, 
-    "JOB_HISTORY": []
+    "JOB_HISTORY": [],
+    "MANAGER": "null"
 }
 db.hrs.insert(employee);
 
@@ -72,6 +72,26 @@ db.hrs.updateOne({EMPLOYEE_ID: 100},{$set: {SALARY: 23999}});
 
 //6 increase employee's salary by 5% if employee has the lowest salary within job id
 db.hrs.update({ $expr: {$eq: ["$SALARY","$JOB.MIN_SALARY"] },"JOB.JOB_ID": "SH_CLERK" }, { $mul: { SALARY: 1.05 } }, {multi: true})
+
+//7 list the average salary of the department with the most employees and commission is null
+db.hrs.aggregate([
+    {$group: 
+        {
+            _id: "$DEPARTMENT.DEPARTMENT_NAME", 
+            salary: {$sum: "$SALARY"},
+            employees: {$sum: 1}
+        }
+    },
+    {
+        $project: {_id:1,employees:1,avg: {$divide: ["$salary","$employees"]}}
+    },
+    {
+        $sort: {employees: -1}
+    },
+    {
+        $limit: 1
+    }
+]);
 
 //8 top 3 countries with higher ratio between salary and n of employees
 db.hrs.aggregate([
